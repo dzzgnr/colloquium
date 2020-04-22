50a26f36e5d92ba7f70815fc41c3aacd027e33a2
Daniil Bondarenko <60926884+dzzgnr@users.noreply.github.com>
2020-04-22 14:43:19 +0300

    4.py

---
+++ 4.py
@@ -0,0 +1,24 @@
+4. Створіть масив з п'яти прізвищ і виведіть на екран ті з них, які
+починаються з певної букви, яка вводиться з клавіатури.
+'''
+
+arr = [] #инициализация массива
+
+print('enter 5 surnames : ')
+
+for i in range(5):
+    el = input() #ввод фамилии
+    arr.append(el) #добавление в массив
+
+print('\narr :', arr)
+
+print('\nenter the letter for which surname will be displayed : ')
+
+letter = input() #ввод буквы
+
+print('\n')
+
+for i in range(5):
+    if (arr[i])[0] == letter: #если первая буква фамилии совпадает с вводом - выводим фамилию
+        print(arr[i])
+
